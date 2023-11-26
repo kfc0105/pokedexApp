4a405c45e4034648dcf862fe37bfd966bf763f0a
ethan2000liu <ethan2000liu@gmail.com>
2023-11-25 21:25:35 -0800

    added comment

--- src/PokeList.tsx
+++ src/PokeList.tsx
@@ -5,12 +5,15 @@ import { Pokemon, PokemonList, PokemonDetails } from './types';
 import './PokeList.css';
 
 const PokeList: React.FC = () => {
+  // State to store the list of Pokemon and the currently selected Pokemon
   const [pokemonList, setPokemonList] = useState<PokemonList | null>(null);
   const [selectedPokemon, setSelectedPokemon] = useState<PokemonDetails | null>(null);
 
+  // Fetch Pokemon data when the component mounts
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // Fetch all Pokemon from the PokeAPI
         let allPokemon: PokemonDetails[] = [];
         let nextUrl = 'https://pokeapi.co/api/v2/pokemon';
         while (nextUrl) {
@@ -19,21 +22,25 @@ const PokeList: React.FC = () => {
           nextUrl = response.data.next;
         }
 
+        // Format the fetched data into a simpler Pokemon array
         const formattedPokemon: Pokemon[] = allPokemon.map((pokemon, index) => ({
           id: index + 1,
           name: pokemon.name,
           url: pokemon.url,
         }));
 
+        // Set the formatted Pokemon list to the state
         setPokemonList({ count: formattedPokemon.length, results: formattedPokemon });
       } catch (error) {
         console.error('Error fetching Pokemon data:', error);
       }
     };
 
+    // Call the fetchData function
     fetchData();
-  }, []);
+  }, []); // The empty dependency array ensures that this effect runs only once, similar to componentDidMount
 
+  // Function to handle clicks on a Pokemon, fetching and setting details
   const handlePokemonClick = async (url: string) => {
     try {
       const response = await axios.get(url);
@@ -43,9 +50,12 @@ const PokeList: React.FC = () => {
     }
   };
 
+  // Function to handle sorting of the Pokemon list based on ID or name
   const handleSort = (option: string) => {
     if (pokemonList) {
+      // Create a copy of the current list
       const sortedList = [...pokemonList.results];
+      // Sort the list based on the selected option (ID or name)
       sortedList.sort((a, b) => {
         if (option === 'id') {
           return a.id - b.id;
@@ -54,21 +64,27 @@ const PokeList: React.FC = () => {
         }
         return 0;
       });
+      // Update the Pokemon list in the state with the sorted list
       setPokemonList({ ...pokemonList, results: sortedList });
     }
   };
 
+  // Render the component
   return (
     <div className="poke-container">
+      {/* Pokemon list section */}
       <div className="pokemon-list">
         <h1>Pokemon List</h1>
         <div>
+          {/* Buttons for sorting the list */}
           <button onClick={() => handleSort('id')}>Sort by ID</button>
           <button onClick={() => handleSort('name')}>Sort by Name</button>
         </div>
+        {/* Render the Pokemon list if available */}
         {pokemonList && (
           <ul>
             {pokemonList.results.map((pokemon) => (
+              // Render each Pokemon item with an onClick event to handle clicks
               <li key={pokemon.name} onClick={() => handlePokemonClick(pokemon.url)}>
                 <img
                   src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
@@ -80,10 +96,13 @@ const PokeList: React.FC = () => {
           </ul>
         )}
       </div>
+
+      {/* Pokemon details section */}
       <div className="pokemon-details">
         {selectedPokemon && (
           <div className="details-container">
             <h2>Details for {selectedPokemon.name}</h2>
+            {/* Display details for the selected Pokemon */}
             <img
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedPokemon.id}.png`}
               alt={selectedPokemon.name}
@@ -100,4 +119,5 @@ const PokeList: React.FC = () => {
   );
 };
 
+// Export the component
 export default PokeList;
